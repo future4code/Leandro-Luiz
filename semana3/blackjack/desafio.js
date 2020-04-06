4a967330ed80fd0746dc08c7fb9bc1ed01497fe7
Fubaluiz1983 <fubaluiz@yahoo.com.br>
2020-04-05 23:04:55 -0300

    Incluído desafio 10

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -48,20 +48,37 @@ if(confirm("Quer iniciar uma nova rodada?")) {
       pcCartaTexto[i] = carta.texto;
       pcCarta1 = pcCarta1 + pcCarta[i];
    }
+   
+   if(confirm("Suas cartas são " + usuarioCartaTexto[0] + usuarioCartaTexto[1] + 
+           ". A carta revelada do computador é " 
+            + pcCartaTexto[0] +".\n"+  // \n faz pular a linha
+           "Deseja comprar mais uma carta?"))
+   {
+      carta = comprarCarta();
+      usuarioCarta[i] = carta.valor;
+      usuarioCartaTexto[i] = carta.texto;
+      usuarioCarta1 = usuarioCarta1 + usuarioCarta[i];       
+   
+    
+            if (usuarioCarta1 < 21){
+               usuarioCarta[i] = carta.valor;
+               usuarioCartaTexto[i] = carta.texto;
+               usuarioCarta1 = usuarioCarta1 + usuarioCarta[i];
+            }
+         }
 
-
-   if(usuarioCarta1 > pcCarta1){
-      console.log("Usuário - cartas:" + usuarioCartaTexto[i] + " - pontuação " + usuarioCarta1);
-      console.log("Computador - cartas:" + pcCartaTexto[i] + " - pontuação " + pcCarta1);
+   if(usuarioCarta1 <= 21 && pcCarta1 < 21 || pcCarta1 > 21){
+      console.log("Usuário - cartas:" + usuarioCartaTexto[0] + usuarioCartaTexto[1] + usuarioCartaTexto[2] + " - pontuação " + usuarioCarta1);
+      console.log("Computador - cartas:" + pcCartaTexto[0] + pcCartaTexto[1] + " - pontuação " + pcCarta1);
       console.log("O usuário ganhou!");
 
-   }else if (pcCarta1 > usuarioCarta1){
-      console.log("Computador - cartas:" + pcCartaTexto[i] + " - pontuação " + pcCarta1);
-      console.log("Usuário - cartas:" + usuarioCartaTexto[i] + " - pontuação " + usuarioCarta1);
+   }else if (pcCarta1 <= 21 && usuarioCarta1 < 21 || usuarioCarta1 > 21){
+      console.log("Computador - cartas:" + pcCartaTexto[0] + pcCartaTexto[1] + " - pontuação " + pcCarta1);
+      console.log("Usuário - cartas:" + usuarioCartaTexto[0] + usuarioCartaTexto[1] + usuarioCartaTexto[2] + " - pontuação " + usuarioCarta1);
       console.log("O Computador ganhou!"); 
    }else{
-      console.log("Computador - cartas:" + pcCartaTexto[i] + " - pontuação " + pcCarta1);
-      console.log("Usuário - cartas:" + usuarioCartaTexto[i] + " - pontuação " + usuarioCarta1);
+      console.log("Computador - cartas:" + pcCartaTexto[0] + pcCartaTexto[1] + pcCartaTexto[2] + " - pontuação " + pcCarta1);
+      console.log("Usuário - cartas:" + usuarioCartaTexto[0] + usuarioCartaTexto[1] + usuarioCartaTexto[2] + " - pontuação " + usuarioCarta1);
       console.log("Jogue novamente empatou");
    } 
  } else {
