53530ca7ac031d662fe9622c791385c8069ce13f
Fubaluiz1983 <fubaluiz@yahoo.com.br>
2020-04-05 22:24:40 -0300

    Retratando o Blackjack

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -11,3 +11,59 @@
  * 
  */
 
+
+
+console.log("Bem vindo ao jogo BlackJack");
+let carta = 0;
+//let valor = 0;
+//let texto = "";
+let i = 0;
+let usuarioCarta = [];
+let usuarioCarta1 = 0;
+let usuarioCartaTexto = [];
+
+let pcCarta = [];
+let pcCarta1 = 0;
+let pcCartaTexto = [];
+
+
+if(confirm("Quer iniciar uma nova rodada?")) {
+   for(i=0; i < 2; i++){   
+   carta = comprarCarta();
+   usuarioCarta[i] = carta.valor;
+   usuarioCartaTexto[i] = carta.texto;
+   usuarioCarta1 = usuarioCarta1 + usuarioCarta[i];
+   }
+      if(usuarioCarta[0] == 11 && usuarioCarta[1] == 11){
+         for(i=0; i < 2; i++){   
+            carta = comprarCarta();
+            usuarioCarta[i] = carta.valor;
+            usuarioCartaTexto[i] = carta.texto;
+            usuarioCarta1 = usuarioCarta1 + usuarioCarta[i];
+         }   
+      } 
+   for(i=0; i < 2; i++){   
+      carta = comprarCarta();
+      pcCarta[i] = carta.valor;
+      pcCartaTexto[i] = carta.texto;
+      pcCarta1 = pcCarta1 + pcCarta[i];
+   }
+
+
+   if(usuarioCarta1 > pcCarta1){
+      console.log("Usuário - cartas:" + usuarioCartaTexto[i] + " - pontuação " + usuarioCarta1);
+      console.log("Computador - cartas:" + pcCartaTexto[i] + " - pontuação " + pcCarta1);
+      console.log("O usuário ganhou!");
+
+   }else if (pcCarta1 > usuarioCarta1){
+      console.log("Computador - cartas:" + pcCartaTexto[i] + " - pontuação " + pcCarta1);
+      console.log("Usuário - cartas:" + usuarioCartaTexto[i] + " - pontuação " + usuarioCarta1);
+      console.log("O Computador ganhou!"); 
+   }else{
+      console.log("Computador - cartas:" + pcCartaTexto[i] + " - pontuação " + pcCarta1);
+      console.log("Usuário - cartas:" + usuarioCartaTexto[i] + " - pontuação " + usuarioCarta1);
+      console.log("Jogue novamente empatou");
+   } 
+ } else {
+	console.log("O jogo acabou");
+}
--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -1,28 +1,8 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
-
-// console.log("Bem vindo ao jogo BlackJack");
- 
-// if(confirm("Quer iniciar uma nova rodada?")){ 
-  
-   
-   
-// }else {
-// 	console.log("O jogo acabou");
-// }
-
-
-// const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+// /**
+//  * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+//  * 
+//  * 
+//     const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
     
 //     console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
 //     console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
@@ -31,28 +11,53 @@
 //  * 
 //  */
 
-console.log("Bem vindo ao jogo BlackJack");
 
-let novarodada;
-const usuarioPontos = 0;
-const pcPontos = 0;
-//novarodada = (confirm("pergunta de sim ou não"));
-if (novarodada === false){
-   console.log("O jogo acabou");
-}else{
-   while(novarodada === true){
-      const usuarioCarta1 = comprarCarta;
-      const usuarioCarta2 = comprarCarta; 
-      const pcCarta1 = comprarCarta;
-      const pcCarta2 = comprarCarta;
-      usuarioPontos = usuarioCarta1.valor + usuarioCarta2.valor;
-      pcPontos = pcCarta1.valor + pcCarta2.valor; 
-   }
-   if(usuarioPontos > pcPontos){
-     
-      console.log("O usuário ganhou!");
-   } else (pcPontos > usuarioPontos)
-      console.log("O computador ganhou!");
-}
+// console.log("Bem vindo ao jogo BlackJack");
+// let carta = 0;
+// let valor = 0;
+// let usuarioCarta1 = 0;
+// let usuarioCarta2 = 0;
+// let usuarioCarta = 0;
+// let usuarioCartaTexto1 = "";
+// let usuarioCartaTexto2 = "";
+// let pcCarta1 = 0;
+// let pcCarta2 = 0;
+// let pcCartaTexto1 = "";
+// let pcCartaTexto2 = "";
 
-novarodada = confirm("Quer iniciar uma nova rodada?");
+// if(confirm("Quer iniciar uma nova rodada?")) {
+//    for(let i=0; i < 1; i++){   
+//    carta = comprarCarta();
+//    usuarioCarta1 = usuarioCarta1 + carta.valor;
+//    usuarioCartaTexto1 = usuarioCartaTexto1 + carta.texto;
+//    carta = comprarCarta();
+//    usuarioCarta2 = usuarioCarta2 + carta.valor;
+//    usuarioCartaTexto2 = usuarioCartaTexto2 + carta.texto;
+//    usuarioCarta = usuarioCarta1 + usuarioCarta2;
+//    }
+
+//    for(let i=0; i < 1; i++){
+//       carta = comprarCarta();
+//       pcCarta1 = pcCarta1 + carta.valor;
+//       pcCartaTexto1 = pcCartaTexto1 + carta.texto;
+//       carta = comprarCarta();
+//       pcCarta2 = pcCarta2 + carta.valor;
+//       pcCartaTexto2 = pcCartaTexto2 + carta.texto;
+//       pcCarta = pcCarta1 + pcCarta2;
+//       }
+//    if(usuarioCarta > pcCarta){
+//       console.log("Usuário - cartas:" + usuarioCartaTexto1 + " " + usuarioCartaTexto2 + " - pontuação " + usuarioCarta);
+//       console.log("Computador - cartas:" + pcCartaTexto1 + " " + pcCartaTexto2 + " - pontuação " + pcCarta);
+//       console.log("O usuário ganhou!");
+//    }else if (pcCarta > usuarioCarta){
+//       console.log("Computador - cartas:" + pcCartaTexto1 + " " + pcCartaTexto2 + " - pontuação " + pcCarta);
+//       console.log("Usuário - cartas:" + usuarioCartaTexto1 + " " + usuarioCartaTexto2 + " - pontuação " + usuarioCarta);
+//       console.log("O Computador ganhou!"); 
+//    }else{
+//       console.log("Computador - cartas:" + pcCartaTexto1 + " " + pcCartaTexto2 + " - pontuação " + pcCarta);
+//       console.log("Usuário - cartas:" + usuarioCartaTexto1 + " " + usuarioCartaTexto2 + " - pontuação " + usuarioCarta);
+//       console.log("Jogue novamente empatou");
+//    } 
+// } else {
+// 	console.log("O jogo acabou");
+// }
