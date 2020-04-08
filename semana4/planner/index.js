c5ee7da9f976acf41c3cdc752d1ca0cca26392cd
Fubaluiz1983 <fubaluiz@yahoo.com.br>
2020-04-07 23:50:32 -0300

    Complementando o recurso da agenda

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -2,34 +2,31 @@ function adicionarTarefa(){
     const novaTarefa = document.getElementById("tarefaNova").value;
     const selecaoDoDia = document.getElementById("selecaoDia").value;
     
-    //document.getElementById("domingo").innerHTML = "<p>" + novaTarefa + "</p>";
-
-    //minhaTarefa.innerHTML = minhaTarefa + "<li>" + novaTarefa.value + "<li>";
 
     if(novaTarefa == " " || novaTarefa == ""){
         console.log(alert("Digitar Tarefa Válida"));
     }else
     
     if (selecaoDoDia == 'domingo') {
-         document.getElementById('domingo').innerHTML = "Domingo" + "<p>" + novaTarefa + "</p>";
+         document.getElementById('domingo').innerHTML += "\n<span>" + novaTarefa + "</span>\n\n/";
 
 	} else if (selecaoDoDia == 'segunda'){
-        document.getElementById('segunda').innerHTML = "Segunda-Feira" + "<p>" + novaTarefa + "</p>";
+        document.getElementById('segunda').innerHTML += "\n<span>" + novaTarefa + "</span>\n\n/";
         
 	}else if (selecaoDoDia == 'terca') {
-        document.getElementById('terca').innerHTML = "Terça-Feira" + "<p>" + novaTarefa + "</p>";
+        document.getElementById('terca').innerHTML += "\n<span>" + novaTarefa + "</span>\n\n/";
         
 	} else if (selecaoDoDia == 'quarta') {
-        document.getElementById('quarta').innerHTML = "Quarta-Feira" + "<p>" + novaTarefa + "</p>";
+        document.getElementById('quarta').innerHTML += "\n<span>" + novaTarefa + "</span>\n\n/";
         
 	} else if (selecaoDoDia == 'quinta') {
-        document.getElementById('quinta').innerHTML = "Quinta-Feira" + "<p>" + novaTarefa + "</p>";
+        document.getElementById('quinta').innerHTML += "\n<span>" + novaTarefa + "</span>\n\n/";
         
 	} else if (selecaoDoDia == 'sexta') {
-        document.getElementById('sexta').innerHTML = "Sexta-Feira" + "<p>" + novaTarefa + "</p>";
+        document.getElementById('sexta').innerHTML += "\n<span>" + novaTarefa + "</span>\n\n/";
         
 	} else if (selecaoDoDia == 'sabado') {
-        document.getElementById('sabado').innerHTML = "Sábado" + "<p>" + novaTarefa + "</p>";
+        document.getElementById('sabado').innerHTML += "\n<span>" + novaTarefa + "</span>\n\n/";
         
 	}
     document.getElementById('tarefaNova').value = "";
