6da35e6e43ef49c0619b219b8267c372dbe6ca92
Fubaluiz1983 <fubaluiz@yahoo.com.br>
2020-04-12 20:34:44 -0300

    Aula 14 - Ex leitura de Código - 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -3,3 +3,9 @@
 // Exercicio de Leitura de Código
 
 // 1- Resposta: R$500
+
+//2- Resposta: 
+    // 165
+    // Vai mostrar a tela de Alerta com a mensagem "Tipo de Investimento informado incorreto!"
+
+    
